b967c419bc7a7df2972fdb0aea2696fa694a2380
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-01-24 14:22:24 +0900

    version 0.9.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/UniDic-COMBO"
 
 setuptools.setup(
   name="unidic_combo",
-  version="0.9.7",
+  version="0.9.8",
   description="UniDic2UD + COMBO-pytorch wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
