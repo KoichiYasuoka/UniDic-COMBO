6ef361fa5100b150a78275aacc43e05b6a05c809
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2024-11-20 19:46:47 +0900

    version 1.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/UniDic-COMBO"
 
 setuptools.setup(
   name="unidic_combo",
-  version="1.4.2",
+  version="1.4.3",
   description="UniDic2UD + COMBO-pytorch wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
   keywords="NLP Japanese spaCy",
   packages=setuptools.find_packages(),
   install_requires=[
-    "unidic2ud>=3.0.2",
+    "unidic2ud>=3.0.6",
     "spacy>=2.2.2",
     "allennlp>=1.2.0",
     "torch>=1.6.0",
@@ -26,7 +26,7 @@ setuptools.setup(
     "requests>=2.23.0",
     "overrides>=3.1.0,<5",
     "protobuf>=3.14.0",
-    "fugashi>=1.3.0",
+    "fugashi>=1.3.3",
     "ipadic>=1.0.0",
     "unidic-lite>=1.0.8",
     "cached_path<=1.1.2"
